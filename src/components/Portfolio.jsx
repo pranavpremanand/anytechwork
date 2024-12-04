2bdaf0002b1c2d35194292fbd7be2723d879be92
Pranav <mpranavprem@gmail.com>
2024-12-04 17:01:25 +0530

    optimised performance

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -22,10 +22,15 @@ const Portfolio = ({ page }) => {
                 <img
                   src={item.img}
                   alt=""
+                  width="300"
+                  height="300"
+                  loading="lazy"
                   className="w-fit h-fit object-contain group-hover:scale-110 transition-all duration-300"
                 />
                 <div className="absolute bottom-0 left-0 w-full p-4 bg-[#1e1b42bb] flex flex-col gap-2">
-                  <h3 className="font-medium text-center text-white text-xl">{item.title}</h3>
+                  <h3 className="font-medium text-center text-white text-xl">
+                    {item.title}
+                  </h3>
                 </div>
               </div>
             ))}
